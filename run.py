e116a676fb6c9029ada4eb7045b5dfb722f65554
Markus-Hefner <m5zrlnlt@students.codeinstitute.net>
2024-11-06 09:53:41 +0000

    Edit add_new_piece function to include mandatory title input and confirmation of piece details (not done yet)

--- run.py
+++ run.py
@@ -24,42 +24,51 @@ def add_new_piece():
     """
     Adds a new piece to spreadsheet
     """
-    new_piece = []
-    index_number = get_index_number() + 1
-    new_piece.append(index_number)
-
     while True:
-        print("Please enter the title of the new piece you wish to add.\n Confirm with 'Enter'.")
-
+        print("Please enter the title of the new piece you wish to add.\nPress 'Enter' to confirm.")
         title = input("Title:\n")
 
-        print("Please enter the composer of the new piece you wish to add. (Optional)\n Confirm with 'Enter'.")
+        # Check if the title already exists in the spreadsheet
+        if any(title.lower() in row[1].lower() for row in data):
+            print("This piece already exists in your repertoire. Please enter a new title.")
+            print("Would you like to enter this piece a second time?")
+            input("Type 'y' for yes and press 'Enter'. Otherwise type 'n' for no and press 'Enter':\n")
+            continue
+        # Check if the title is empty
+        elif title == "":
+            print("Title cannot be empty. Please enter a title.")
+            continue
+        else:
+            break
 
-        composer = input("Composer:\n")
+    print("\nPlease enter the composer of the new piece you wish to add. (Optional)\nPress 'Enter' to confirm.")
+    composer = input("Composer:\n")
+    print("\nPlease enter the arranger of the new piece you wish to add. (Optional)\nPress 'Enter' to confirm.")
+    arranger = input("Arranger:\n")
+    print("\nPlease enter additional information of the new piece you wish to add. (Optional)\nPress 'Enter' to confirm.")
+    additional_info = input("Additional information:\n")
 
-        print("Please enter the arranger of the new piece you wish to add. (Optional)\n Confirm with 'Enter'.")
+    print("\nPlease confirm the following details are correct:\n")
+    print(f"Title: {title}")
+    print(f"Composer: {composer}")
+    print(f"Arranger: {arranger}")
+    print(f"Additional information: {additional_info}\n")
 
-        arranger = input("Arranger:\n")
+    while True:
+        confirmation = input("Type 'y' for yes and press 'Enter'. Otherwise type 'n' for no and press 'Enter':\n")
+        if confirmation.lower() == "y":
+            print("\nNew piece has been added to your repertoire\n")
+            break
+        elif confirmation.lower() == "n":
+            print("\nPlease re-enter the details.\n")
+            add_new_piece()
+        else:
+            print("\nInvalid input.\n")
 
-        print("Please enter additional information of the new piece you wish to add. (Optional)\n Confirm with 'Enter'.")
+    new_piece = []
+    index_number = get_index_number() + 1
 
-        additional_info = input("Additional information:\n")
-
-        break
-    #   print("Please confirm the following details are correct:\n")
-    #   print(f"Title: {title}\n")
-    #   print(f"Composer: {composer}\n")
-    #   print(f"Arranger: {arranger}\n")
-    #   print(f"Additional information: {additional_info}\n")
-
-    #   confirm = input("Please confirm the details are correct. (Y/N)\n")
-    #  if confirm.lower() == "y":
-    #      break
-    #  else:
-    #      print("Please re-enter the details.\n")
-    #      continue
-    #   break
-
+    new_piece.append(index_number)
     new_piece.append(title)
     new_piece.append(composer)
     new_piece.append(arranger)
@@ -67,6 +76,16 @@ def add_new_piece():
 
     add_piece_to_index(new_piece)
     add_new_worksheet(new_piece)
+    
+    # practice_adding_or_index() (Comment in in final version. Commented out to not get into an endless loop while testing)
+
+# def search_piece():
+#     """
+#     Searches for a piece in the spreadsheet
+#     """
+#     print("Please enter the title of the piece you wish to search for.\nPress 'Enter' to confirm.")
+#     search_title = input("Title:\n")
+    
 
 def get_index_number():
     """
@@ -129,7 +148,7 @@ def show_repertoire():
         
 
 
-practice_adding_or_index()
+practice_adding_or_repertoire()
 
 
 
